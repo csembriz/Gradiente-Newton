2cfc5c3fe5e0975a63fc6a5786a50ac6d7f25071
casvel <casvel.d@gmail.com>
2015-05-12 22:27:57 -0500

    Modificación para crear .pdf con latex

--- src/Ascenso.py
+++ src/Ascenso.py
@@ -3,13 +3,16 @@
 from __future__ import absolute_import, division
 from sympy import *
 from findOptimum import *
+from Parse import *
+from pylatex import Document, Section, Math, Package
+import os
 
 def conditional(nabla):
 	evalu = [ True if abs(N(nab)) > 10e-6 else False for nab in nabla ]
 	return sum(evalu)
 
 def Ascenso(objective, varbls, f, punto):
-	l      = Symbol("lamb")
+	l      = Symbol("r")
 	varbls = [ Symbol(var) for var in varbls if var in f ]
 	f      = Lambda(varbls, f)
 
@@ -21,29 +24,69 @@ def Ascenso(objective, varbls, f, punto):
 	nabla_eval = Matrix([ Nab(*flatten(punto)) for Nab in Nabla ])
 
 	out = open("../data/data.out", "w")
+
+	### Para crear el .pdf con latex
+	doc = Document()
+	doc.packages.append(Package('geometry', options=['margin=1in']))
+	doc.packages.append(Package('inputenc', options=['utf8']))
+	doc.packages.append(Package('babel', options=['spanish', 'activeacute']))
+	doc.packages.append(Package('enumerate'))
+	doc.packages.append(Package('amsthm'))
+	doc.packages.append(Package('amssymb'))
+	doc.packages.append(Package('amsmath'))
+	doc.append('\\decimalpoint')
+
+	doc.append('Maximizar:' if objective else 'Minimizar:')
+	doc.append(Math(data = ['f('+ parseVarbls(varbls) + ')=' + parseFunction(f, varbls)] ))
+	doc.append('Derivando la función:')
+
+	doc.append('\\begin{align*}')
 	for i in range(len(Nabla)):
 		out.write('D_' + str(varbls[i]) + " = " + str(Nabla[i](*flatten(varbls))).replace('**', '^') + '\n' )
+		doc.append('\\nabla f_{' + str(varbls[i]) + "} & = " + parseFunction(Nabla[i], varbls) + '\\\\' )
+	doc.append('\\end{align*}')
+
+	doc.append('Tomamos como punto inicial $p_0 =('+parseVarbls(list(punto))+')$.\
+		Iterando con el método del \\textit{ascenso/descenso más pronunciado} obtenemos:$$$$')
 
 	condition = conditional(nabla_eval)
 
 	while(condition > 0):
+
+		### latex
+		doc.append('\\textbf{Iteración '+str(count)+'.}\\\\')
+		doc.append('Evaluamos $\\nabla f$ en el punto $p_{'+str(count-1)+'}=(' + parseVarbls([round(c, 3) for c in punto]) + \
+			')$, entonces $\\nabla f =(' + parseVarbls([round(c, 3) for c in nabla_eval]) + ')$. Obteniendo $h(r)$:')
+
+
 		h = Lambda(l, f(*flatten(punto + l*nabla_eval)))
-
 		l_nopt = findOptimum(h, objective)
-
 		punto = punto + l_nopt*nabla_eval
 
-		out.write("\nIteración " + str(count))
+
+		### salida
+		out.write("\nIteración " + str(count)+'\\\\')
 		out.write("\n\nNabla(p_" + str(count - 1) + ") = " +  str(list(nabla_eval)) + "\n")
 		out.write(("\nMAX " if objective == 1 else "\nMIN ") + "h(r)=" + str(h(l)).replace('**', '^'))
 		out.write("\nlamb = " + str(l_nopt) + "\n")
 		out.write('\np_' + str(count) + ' = ' + str(list(punto)) + '\n')
 
-		nabla_eval = Matrix([ N(Nab(*flatten(punto))) for Nab in Nabla ])
+		
+		### latex
+		doc.append(Math(data=['h(r)='+parseFunction(h, [l])]))
+		doc.append(('Maximizando' if objective else 'Minimizando')+' $h$ encontramos $r$='+str(round(l_nopt, 3))+'.\
+			Así $p_{'+str(count)+'}$=('+parseVarbls([round(c, 3) for c in punto])+').$$$$')
 
+
+		nabla_eval = Matrix([ N(Nab(*flatten(punto))) for Nab in Nabla ])
 		condition = conditional(nabla_eval)
 		count = count + 1
 
+	### latex
+	doc.append('Por lo tanto el punto '+('máximo' if objective else 'mínimo')+' es: ('+parseVarbls([round(c, 3) for c in punto])+')')
+	doc.generate_pdf()
+	os.system('okular default_filename.pdf &')
+ 
 	out.close()
 
 
--- src/conjugados.py
+++ src/conjugados.py
@@ -3,14 +3,16 @@
 from __future__ import absolute_import, division
 from sympy import *
 from findOptimum import *
-
+from Parse import *
+from pylatex import Document, Section, Math, Package
+import os
 
 def conditional(nabla):
 	evalu = [ True if abs(N(nab)) > 10e-6 else False for nab in nabla ]
 	return sum(evalu)
 
 def conjugados(objective, varbls, f, punto):
-	l      = Symbol("lamb")
+	l      = Symbol("r")
 	varbls = [ Symbol(var) for var in varbls if var in f ]
 	f      = Lambda(varbls, f)
 
@@ -22,16 +24,39 @@ def conjugados(objective, varbls, f, punto):
 	nabla_eval = Matrix([ Nab(*flatten(punto)) for Nab in Nabla ])
 
 	out = open("../data/data.out", "w")
+
+	### Para crear el .pdf con latex
+	doc = Document()
+	doc.packages.append(Package('geometry', options=['margin=1in']))
+	doc.packages.append(Package('inputenc', options=['utf8']))
+	doc.packages.append(Package('babel', options=['spanish', 'activeacute']))
+	doc.packages.append(Package('enumerate'))
+	doc.packages.append(Package('amsthm'))
+	doc.packages.append(Package('amssymb'))
+	doc.packages.append(Package('amsmath'))
+	doc.append('\\decimalpoint')
+
+	doc.append('Maximizar:' if objective else 'Minimizar:')
+	doc.append(Math(data = ['f('+ parseVarbls(varbls) + ')=' + parseFunction(f, varbls)] ))
+	doc.append('Derivando la función:')
+
+	doc.append('\\begin{align*}')
 	for i in range(len(Nabla)):
 		out.write('D_' + str(varbls[i]) + " = " + str(Nabla[i](*flatten(varbls))).replace('**', '^') + '\n' )
+		doc.append('\\nabla f_{' + str(varbls[i]) + "} & = " + parseFunction(Nabla[i], varbls) + '\\\\' )
+	doc.append('\\end{align*}')
 	
 	condition = conditional(nabla_eval)
 
 	while(condition > 0):
 
+
 		maxi = -10e10
-		for delt in nabla_eval:
-			maxi = max(maxi, abs(delt))
+		ind  = 0
+		for i in range(0, len(nabla_eval)):
+			if (abs(nabla_eval[i]) > maxi):
+				maxi = abs(nabla_eval[i])
+				ind  = i
 
 		done = False
 		for i in range(0, len(nabla_eval)):
@@ -40,24 +65,41 @@ def conjugados(objective, varbls, f, punto):
 			else:
 				done = True
 
-		h = Lambda(l, f(*flatten(punto + l*nabla_eval)))
-		#l_nopt = solve(sympify(h_diff(l)), l)
-		l_nopt = findOptimum(h, objective)
-		#print(l_nopt)
-		#print("IC:", findOptimum(h, objective))
 
+		### latex
+		doc.append('\\textbf{Iteración '+str(count)+'.}\\\\')
+		doc.append('Evaluamos $\\nabla f$ en el punto $p_{'+str(count-1)+'}=(' + parseVarbls([round(c, 3) for c in punto]) + \
+			')$, entonces $\\nabla f =(' + parseVarbls([round(c, 3) for c in nabla_eval]) + ')$. Encontramos que el máximo delta'+ \
+			' se encuentra en $'+str(varbls[ind])+'$. Obteniendo $h(r)$:')
+
+
+		h = Lambda(l, f(*flatten(punto + l*nabla_eval)))
+		l_nopt = findOptimum(h, objective)
 		punto = punto + l_nopt*nabla_eval
 
+
+		### salida
 		out.write("\nIteración " + str(count))
 		out.write("\n\nNabla(p_" + str(count - 1) + ") = " +  str(list(nabla_eval)) + "\n")
 		out.write(("\nMAX " if objective == 1 else "\nMIN ") + "h(lamb)=" + str(h(l)).replace('**', '^'))
 		out.write("\nlamb = " + str(l_nopt) + "\n")
 		out.write('\np_' + str(count) + ' = ' + str(list(punto)) + '\n')
 
+
+		### latex
+		doc.append(Math(data=['h(r)='+parseFunction(h, [l])]))
+		doc.append(('Maximizando' if objective else 'Minimizando')+' $h$ encontramos $r$='+str(round(l_nopt, 3))+'.\
+			Así $p_{'+str(count)+'}$=('+parseVarbls([round(c, 3) for c in punto])+').$$$$')
+
 		nabla_eval = Matrix([ N(Nab(*flatten(punto))) for Nab in Nabla ])
 
 		condition = conditional(nabla_eval)
 		count     = count + 1
+
+	### latex
+	doc.append('Por lo tanto el punto '+('máximo' if objective else 'mínimo')+' es: ('+parseVarbls([round(c, 3) for c in punto])+')')
+	doc.generate_pdf()
+	os.system('okular default_filename.pdf &')
 
 	out.close()
 
